91cd0d9e72d5b7a58d7225affd84e0057abc898e
ellifas <ellifasr@outlook.com>
2022-08-25 20:19:36 -0300

    att .py

--- Ouvidoria_ellifas_rafael.py
+++ Ouvidoria_ellifas_rafael.py
@@ -14,18 +14,35 @@ if usuario_bd == usuario and senha_bd == senha:
 else:
     estalogado = False
     print('Usuário ou senha inválidas')
+ocorrencias =[]
+ids = []
 if estalogado == True:
     while True:
-        print('1.cdstoc,2.listoc,3.apagaroc,4.sair')
+        print('1.Cadastrar Ocorrência,2.Listar Ocorrências,3.Apagar Ocorrências,4.Sair')
         opc = int(input('Escolha a opção'))
         if opc == 1:
-            print('opção1')
+            print('Cadastrar ocorrências')
+            id += 1
+            nome_pessoa = str(input("Digite o seu nome"))
+            cpf_pessoa = str(input("Digte o seu cpf"))
+            descricao = str(input("Digite a sua ocorrência"))
+            ocorrencia = { "id":1, 'Nome' : nome_pessoa,
+                          "CPF" : cpf_pessoa,
+                          'Ocorrência' : descricao }
+            ocorrencias.append(ocorrencia)
+            ids.append(id)
         elif opc == 2:
-            print('opção2')
+            print(ocorrencias)
         elif opc == 3:
-            print('opção3')
+            print('Apagar Ocorrência')
+            id_remove = int(input("Digite o id da ocorrência que você deseja deletar"))
+            for i in range(len(ocorrencias)):
+                if ocorrencias[i][id] == id_remove:
+                    ocorrencias.pop(i)
+                print("Ocorrência Apagada")
         elif opc == 4:
-            print('opção4')
+            break
+            print('Sair')
         else:
             print('Opção invalida')
 
