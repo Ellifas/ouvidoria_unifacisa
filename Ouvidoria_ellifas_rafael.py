d526c629cb14501cf9e145f465435cebf206ae55
Ellifas <ellifasr@outlook.com>
2022-08-27 17:04:38 -0300

    att .py add feedback

--- Ouvidoria_ellifas_rafael.py
+++ Ouvidoria_ellifas_rafael.py
@@ -1,9 +1,9 @@
 print("Seja bem vindo a nossa ouvidoria!")
-print("made by elliascorp")
+print("made by ELR")
 #coloquei usuário e senha só pq achei que ficaria mais organizado :D
 estalogado = False
 usuario = input('nome do usuário: ')
-senha = input('senha do usuário:')
+senha = input('senha do usuário: ')
 
 usuario_bd = 'ellias'
 senha_bd = 'ellias123'
@@ -17,28 +17,48 @@ else:
 ocorrencias =[]
 contador = 1
 if estalogado == True:
+  #Achei bom manter o nome e cpf para ter uma descrição da pessoa e se não é trote, tendo em vista que pode-se ter verificação policial
+  #Gostaria que fosse uma ouvidoria geral da prefeitura, para por problemas em geral e principalmente segurança, energia e agua, as próximas versões vão estar mais completas.
     while True:
         print('1.Cadastrar Ocorrência,2.Listar Ocorrências,3.Apagar Ocorrências,4.Sair')
-        opc = int(input('Escolha a opção'))
+        opc = int(input('Escolha a opção:\n'))
         if opc == 1:
             print('Cadastrar ocorrências')
-            nome_pessoa = str(input("Digite o seu nome"))
-            cpf_pessoa = str(input("Digte o seu cpf"))
-            descricao = str(input("Digite a sua ocorrência"))
-            ocorrencia = { "id": contador, 'Nome' : nome_pessoa,
+            nome_pessoa = str(input("Digite o seu nome:\n"))
+            cpf_pessoa = str(input("Digte o seu cpf:\n"))
+            descricao = str(input("Digite a sua ocorrência:\n"))
+            ocorrencia = { "id": contador,'Nome' : nome_pessoa,
                           "CPF" : cpf_pessoa,
-                          'Ocorrência' : descricao }
+                          'Ocorrência' : descricao}
             ocorrencias.append(ocorrencia)
             contador += 1
+            for opc in ocorrencias:
+                print("Você poderia deixar um feedback da nossa ouvidoria?")
+                fdbk = (int(input("Caso queira, digite 1!\n")))
+                if fdbk == 1:
+                    input("Deixe aqui o seu feedback!\n")
+                    print("Obrigado pelo o feedback e por usar nossa ouvidoria!")
+                else:
+                    print("Obrigado por usar nossa ouvidoria!")
         elif opc == 2:
-            print(ocorrencias)
+            for value in ocorrencia.values():
+                print(value)
         elif opc == 3:
-            print('Apagar Ocorrência')
-            id_remove = int(input("Digite o id da ocorrência que você deseja deletar"))
-            for i in range(len(ocorrencias)):
-                if ocorrencias[i][id] == id_remove:
-                    ocorrencias.pop(i)
-                print("Ocorrência Apagada")
+            while True:
+                apg = int(input("1. Para apagar todas as ocorrências \n2. Para apagar uma ocorrência específica.\n"))
+                if apg == 1:
+                    ocorrencias.clear()
+                    print("Você apagou todas as ocorrências!")
+                    break
+                elif apg == 2:
+                    deletar = int(input("Digite o número da ocorrência que você deseja deletar \n"))
+                    ocorrencias.pop(deletar - 1)
+                    for item in ocorrencias:
+                        print(item)
+                    break
+                else:
+                    print("Não posso rodar essa ação")
+            continue
         elif opc == 4:
             break
             print('Sair')
